4eefb304c7c57160ece886a87154f159d3bb0048
JINQIAHNG1 <18268091316@qq.com>
2022-06-19 16:08:46 +0800

    关于reduce的使用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+
+const arr = [];
+for (let i = 1; i <= 100; i++) {
+  arr.push(i);
+}
+const sum = arr.reduce((previous, current) => previous + current);
+console.log(sum);
+
+const string = "http://www.baidu.com/?name=zhangsan&age=18&sex=nan#";
+const newStr = string.split("?", 2)[1];
+const str = newStr/* .replace(/=/g,':') */.slice(0, newStr.length - 1).split("&")
+const obj = {} 
+str.reduce((pre, cur) => {
+  let a = cur.split('=')
+  console.log(a[0]);
+  obj[a[0]] = a[1]
+},0)
+console.log(obj);
